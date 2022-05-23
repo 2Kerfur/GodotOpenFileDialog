a089b89499c80624ae1f19d51fdfc7d851411515
Biner <73479696+2Kerfur@users.noreply.github.com>
2022-05-23 22:46:05 +0400

    Update OpenFile.cpp

--- OpenFile.cpp
+++ OpenFile.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <string>
+
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem;
+
+int main() {
+    namespace fs = std::experimental::filesystem;
+    std::string mypath { "../MyDir" };
+    if(fs::exists(mypath))
+    {
+
+    }
+}
 
