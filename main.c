58493edb1ff1cff1ef10eb751941972ded787f64
Biner <73479696+2Kerfur@users.noreply.github.com>
2022-05-23 23:27:52 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+#include <stdio.h>
+
+int main() {
+    struct stat st = {0};
+    if (stat("/home/kerfur", &st) == -1) {
+        printf("Does not exist");
+    }
+}
 
