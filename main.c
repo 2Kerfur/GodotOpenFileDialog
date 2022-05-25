09ebcc6c478f2c004fc3cb6d36a804cf489432df
Biner <73479696+2Kerfur@users.noreply.github.com>
2022-05-25 23:43:55 +0400

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,12 @@
 #include <string.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+
+#include <fcntl.h>      // File control definitions
+#include <errno.h>      // Error number definitions
+#include <termios.h>
 
 int dirs_count = 0;
 
@@ -15,9 +21,6 @@ void append_char(char* s, char c) {
 }
 
 int main() {
-
-
-
     //create array for ComPorts
     int dirs_count = 0;
     DIR *ComPortsDir_count;
@@ -49,20 +52,111 @@ int main() {
     printf("-----------\n");
 
     char devices_path[dirs_count][32];
-    for (int i = 0; i < counter_2; i += 1)
+    struct stat st = {0};
+    int counter_3 = 0;
+    for (int i = 2; i < counter_2; i += 1) //starts from 2 because first 2 is . and ..
     {
 
         char str[32] = "/sys/class/tty/";
-        printf(strcat(str, devices[i]));
-        printf("\n");
+        strcat(str, devices[i]);
+        strcat(str, "/device");
+        if (stat(str, &st) == -1) {
+        } else {
+            printf(str);
+            counter_3 += 1;
+            printf("\n");
+        }
+    }
+    printf("%d",counter_3);
+
+
+    //arduino input reading
+    //-----------------------
+
+    int USB = open( "/dev/ttyACM0", O_RDWR| O_NOCTTY );
+
+    struct termios tty;
+struct termios tty_old;
+memset (&tty, 0, sizeof tty);
+
+/* Error Handling */
+if ( tcgetattr ( USB, &tty ) != 0 ) {
+   //std::cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << std::endl;
+}
+
+/* Save old tty parameters */
+tty_old = tty;
+
+/* Set Baud Rate */
+cfsetospeed (&tty, (speed_t)B9600);
+cfsetispeed (&tty, (speed_t)B9600);
+
+/* Setting other Port Stuff */
+tty.c_cflag     &=  ~PARENB;            // Make 8n1
+tty.c_cflag     &=  ~CSTOPB;
+tty.c_cflag     &=  ~CSIZE;
+tty.c_cflag     |=  CS8;
+
+tty.c_cflag     &=  ~CRTSCTS;           // no flow control
+tty.c_cc[VMIN]   =  1;                  // read doesn't block
+tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
+tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+
+/* Make raw */
+cfmakeraw(&tty);
+
+/* Flush Port, then applies attributes */
+tcflush( USB, TCIFLUSH );
+if ( tcsetattr ( USB, TCSANOW, &tty ) != 0) {
+   //std::cout << "Error " << errno << " from tcsetattr" << std::endl;
+}
+    //-----------------------
+
+    while (1)
+    {
+
+        int n = 0,
+    spot = 0;
+char buf = '\0';
+
+/* Whole response*/
+char response[1024];
+memset(response, '\0', sizeof response);
+
+do {
+    n = read( USB, &buf, 1 );
+    sprintf( &response[spot], "%c", buf );
+    spot += n;
+} while( buf != '\r' && n > 0);
+
+if (n < 0) {
+    printf("Error reading: ");
+}
+else if (n == 0) {
+    printf("Read nothing!");
+}
+else {
+    printf("Response: ");
+    printf(response);
+}
     }
 
-
-
-    struct stat st = {0};
-    if (stat("/sys/class/tty/", &st) == -1) {
-        printf("Does not exist");
-    }
 
     return 0;
+}
+
+
+void OpenFileDialog()
+{
+    char filename[1024];
+    FILE *f = popen("zenity --file-selection", "r");
+    fgets(filename, 1024, f);
+    printf(filename);
+}
+void OpenFolderDialog()
+{
+    char filename_2[1024];
+    FILE *f_2 = popen("zenity --file-selection --directory", "r");
+    fgets(filename_2, 1024, f_2);
+    printf(filename_2);
 }
